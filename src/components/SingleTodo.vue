612db8881915f9f3879fdbec984194a9b9b33b0a
maria-jon <mariajonasson93@gmail.com>
2025-01-27 13:54:05 +0100

    feature(todos): add mark as done

--- src/components/SingleTodo.vue
+++ src/components/SingleTodo.vue
@@ -1,12 +1,28 @@
 <script setup lang="ts">
-defineProps({
+import { ref } from 'vue';
+
+const props = defineProps({
   todoText: { type: String, required: true },
   complete: { type: Boolean, default: false },
 });
+
+const isDone = ref(props.complete);
+
+function toggleCompletedState() {
+  isDone.value = !isDone.value;
+}
 </script>
 
 <template>
-  <div :class="{ completed: complete }">{{ todoText }}</div>
+  <div :class="{ completed: isDone }" class="grid grid-cols-2 gap-2">
+    <div>{{ todoText }}</div>
+    <div>
+      <button @click="toggleCompletedState">
+        <span v-if="isDone">Avmarkera som klar</span>
+        <span v-if="!isDone">Markera som klar</span>
+      </button>
+    </div>
+  </div>
 </template>
 
 <style scoped>
