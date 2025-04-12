5eae05a42c09ee98661f6bf1eccadd67977eeda0
maria-jon <mariajonasson93@gmail.com>
2025-04-12 13:37:32 +0200

    fix: add headings and margins to homeview

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -26,13 +26,14 @@ function clearMessage() {
 
 <template>
   <main>
-    <h2>My to-dos</h2>
     <div class="todo-input">
+      <h2>Add new to-do</h2>
       <input type="text" v-model="todoName" @keyup.enter="addTodo" />
       <button @click="addTodo">Add to-do</button>
     </div>
     <div class="todo-list">
-      <p v-if="showCompletedMessage">Well done!</p>
+      <h2>My to-do-list</h2>
+      <p v-if="showCompletedMessage">Well done completing that!</p>
 
       <div v-if="todos.length > 0" v-for="(todo, index) in todos" :key="index">
         <SingleTodo :todo-text="todo.text" :complete="todo.complete" :id="todo.id" @task-completed="onTaskCompleted" />
@@ -44,6 +45,20 @@ function clearMessage() {
 </template>
 
 <style scoped lang="scss">
+main {
+  padding: 1rem;
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+}
+
+.todo-input {
+  align-self: flex-end;
+}
+.todo-input > button {
+  margin-left: 1rem;
+}
+
 .todo-list {
   display: flex;
   flex-direction: column;
