dab892f372689001ac92dcba5118f071b74e2e33
maria-jon <mariajonasson93@gmail.com>
2025-04-12 14:37:24 +0200

    feat: add text content to about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,8 @@
 <template>
   <main class="about">
     <h2>About</h2>
+    <p>To-do-app created with Vite and Vue as a school project.</p>
+    <p>User can add tasks to app, mark them as done, and delete them. To-do-tasks are saved in localStorage.</p>
+    <p>Check it out on <a href="https://github.com/maria-jon/todo">GitHub</a>!</p>
   </main>
 </template>
